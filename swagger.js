374a78d4ec753cc63190e47511cdf8717c9f5ed0
rrachitha <rrajagopal@architech.ca>
2022-06-17 02:43:49 -0400

    re-structure app files

--- swagger.js
+++
@@ -1,9 +0,0 @@
-const swaggerAutogen = require('swagger-autogen')()
-
-const outputFile = './swagger_output.json'
-const endpointsFiles = ['./server.js']
-
-
-swaggerAutogen(outputFile, endpointsFiles).then(() => {
-    require('./index.js')
-})
